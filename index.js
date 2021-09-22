2b38f4ecff761a557e88b4fdd3494cb2eb08930a
Viacheslav Nikitin <dzxt@bk.ru>
2021-09-22 21:51:21 +0700

    Update index.js
    fewfunc

--- index.js
+++ index.js
@@ -17,3 +17,53 @@ function parseHeadlessTable(sel){
   })
   return new Map(values)
 }
+function isCheerio(e) {
+    try {
+        return e.cheerio === '[cheerio object]'
+    } catch (error) { }
+}
+
+
+function is_elements_contains(elements_selector, search_text) {
+    const arr = $(elements_selector).toArray()
+    return !!find_item_in(arr).equal(search_text)
+}
+
+
+verify_requests(r => {
+    if (r.error!='net::ERR_ABORTED' && 
+      reqs_wl.some(w => w.test(r.url)) &&
+      (!r.response || r.response.status != 404))
+    {
+      throw new Error();
+    }
+});
+
+function searchJsonPath(source, name, f, path = '$') {
+if (!source) return { [path]: false }
+//console.log(path)
+if (!Array.isArray(source)) {
+  let results = Object.keys(source).map(key => {
+    if (key == name) {
+
+      f({ [path]: source[key] })
+      if (typeof source[key] == 'string') {
+
+      } else {
+        return searchJsonPath(source[key], name, f, path + '.' + key)
+      }
+    } else if (typeof source[key] == 'object') {
+      return searchJsonPath(source[key], name, f, path + '.' + key)
+    } else {
+      return { [path]: false }
+    }
+  })
+  return results
+} else {
+  let arr = source.map((item, i) => {
+    return searchJsonPath(item, name, f, `${path}[${i}]`)
+  })
+  return { [path]: arr }
+}
+
+}
